e7b24b9ceda23e9566354848650f737395c0cff5
Nezaket Mammedzadeh <100687473+nazmzadeh@users.noreply.github.com>
2023-05-06 23:15:04 +0400

    navbar finished

--- BeBold/assets/js/main.js
+++ BeBold/assets/js/main.js
@@ -1,42 +1,41 @@
-let dropdown=document.querySelector(".dropdown");
-let dropdownList=document.querySelector(".dropdown_list");
-let dropdownOptions=document.querySelectorAll("dropdown_option");
-let hamburger =document.querySelector(".hamburger_icon");
-let mainNav=document.querySelectorAll(".main_nav");
-let rightOptions=document.querySelector(".right_options");
-let responsive=document.querySelector(".responsive");
-let body =document.querySelector("body");
+let dropdown = document.querySelector(".dropdown");
+let dropdownList = document.querySelector(".dropdown_list");
+let dropdownOptions = document.querySelectorAll(".dropdown_option");
+let hamburger = document.querySelector(".hamburger_icon");
+let mainNav = document.querySelectorAll(".main_nav");
+let rightOptions = document.querySelector(".right_options");
+let responsive = document.querySelector(".responsive");
 
-hamburger.onclick=()=>responsive.classList.toggle("responsive_on"); 
+hamburger.onclick = () => {
+    responsive.classList.toggle("responsive_on");
+    hamburger.classList.toggle('active');
+};
+dropdown.onclick = () => {
+   dropdownList.classList.toggle("show");
+};
+// function Dropdown() {
+//     if (window.matchMedia("(max-width:920px)").matches) {
+//         // dropdown.onclick=()=>
 
+//         //   body.style.backgroundColor="red";
 
-// window.addEventListener("resize",function () {
-    
-// })
+//     } else {
+//         //   dropdown.onclick=()=>dropdownList.classList.remove("clicked");
 
-function Dropdown(){
-    if (window.matchMedia("(max-width:920px)").matches) {
-        // dropdown.onclick=()=>dropdownOptions.forEach(item => {
-        //     item.style.display="block";
-        // });
-      dropdown.onclick=()=>{
-        dropdownList.classList.toggle("clicked");
-        dropdownList.style.visibility="visible";
-        dropdownList.style.opacity=1;
-        dropdownOptions.forEach(el => {
-            el.classList+="clicked_options";
-            el.style.visibility="visible";
-            el.style.opacity=1;
-        });
-      }
-    //   body.style.backgroundColor="red";
-        
-    }else{
-      dropdown.onclick=()=>dropdownList.classList.remove("clicked");
-      
-    //   body.style.backgroundColor="white";
+//         //   body.style.backgroundColor="white";
 
-    }
-}
-window.onresize=Dropdown;
-window.onload=Dropdown;
+//     }
+// }
+// dropdown.onclick = () => {
+//     // dropdownList.classList.toggle("clicked");
+//     // dropdownList.style.visibility="visible";
+//     // dropdownList.style.display="block";
+//     // dropdownList.style.opacity=1;
+//     // dropdownOptions.forEach(el => {
+//     //     el.classList+="clicked_options";
+//     //     el.style.visibility="visible";
+//     //     el.style.opacity=1;
+//     // });
+// }
+// window.onresize = Dropdown;
+// window.onload = Dropdown;
