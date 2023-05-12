f185d2f45d7eae7aa256eab42050179fca42f9e0
Nezaket Mammedzadeh <100687473+nazmzadeh@users.noreply.github.com>
2023-05-12 10:10:56 +0400

    problem in navbar fixed and hero-section finished

--- BeBold/assets/js/main.js
+++ BeBold/assets/js/main.js
@@ -1,10 +1,10 @@
 let dropdown = document.querySelector(".dropdown");
 let dropdownList = document.querySelector(".dropdown_list");
-let dropdownOptions = document.querySelectorAll(".dropdown_option");
 let hamburger = document.querySelector(".hamburger_icon");
-let mainNav = document.querySelectorAll(".main_nav");
-let rightOptions = document.querySelector(".right_options");
 let responsive = document.querySelector(".responsive");
+let header=document.querySelector("header");
+const toggleClass = "is-sticky";
+
 
 hamburger.onclick = () => {
     responsive.classList.toggle("responsive_on");
@@ -13,29 +13,12 @@ hamburger.onclick = () => {
 dropdown.onclick = () => {
    dropdownList.classList.toggle("show");
 };
-// function Dropdown() {
-//     if (window.matchMedia("(max-width:920px)").matches) {
-//         // dropdown.onclick=()=>
 
-//         //   body.style.backgroundColor="red";
-
-//     } else {
-//         //   dropdown.onclick=()=>dropdownList.classList.remove("clicked");
-
-//         //   body.style.backgroundColor="white";
-
-//     }
-// }
-// dropdown.onclick = () => {
-//     // dropdownList.classList.toggle("clicked");
-//     // dropdownList.style.visibility="visible";
-//     // dropdownList.style.display="block";
-//     // dropdownList.style.opacity=1;
-//     // dropdownOptions.forEach(el => {
-//     //     el.classList+="clicked_options";
-//     //     el.style.visibility="visible";
-//     //     el.style.opacity=1;
-//     // });
-// }
-// window.onresize = Dropdown;
-// window.onload = Dropdown;
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll > 180) {
+      header.classList.add(toggleClass);
+    } else {
+      header.classList.remove(toggleClass);
+    }
+  });
