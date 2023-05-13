f868b435a443328805ec136f40902b623d27c39a
Nezaket Mammedzadeh <100687473+nazmzadeh@users.noreply.github.com>
2023-05-13 14:49:51 +0400

    added some fixes,slick and unfinished cards section

--- BeBold/assets/js/main.js
+++ BeBold/assets/js/main.js
@@ -2,7 +2,7 @@ let dropdown = document.querySelector(".dropdown");
 let dropdownList = document.querySelector(".dropdown_list");
 let hamburger = document.querySelector(".hamburger_icon");
 let responsive = document.querySelector(".responsive");
-let header=document.querySelector("header");
+let header = document.querySelector("header");
 const toggleClass = "is-sticky";
 
 
@@ -11,14 +11,43 @@ hamburger.onclick = () => {
     hamburger.classList.toggle('active');
 };
 dropdown.onclick = () => {
-   dropdownList.classList.toggle("show");
+    dropdownList.classList.toggle("show");
 };
 
 window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset;
     if (currentScroll > 180) {
-      header.classList.add(toggleClass);
+        header.classList.add(toggleClass);
     } else {
-      header.classList.remove(toggleClass);
+        header.classList.remove(toggleClass);
     }
-  });
+});
+
+$(document).ready(function () {
+    $('.slick_carousel').slick({
+        arrows:false,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+        ]
+    });
+});
