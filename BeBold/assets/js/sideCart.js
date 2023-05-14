62b9cb42e85fb3697a67515f1dfead37e0dc0420
Nezaket Mammedzadeh <100687473+nazmzadeh@users.noreply.github.com>
2023-05-15 03:48:23 +0400

    cart ui and some functionalities added

---
+++ BeBold/assets/js/sideCart.js
@@ -0,0 +1,38 @@
+let closeCart = document.querySelector(".close");
+let openCart = document.querySelector(".cart");
+let sideCartMenu = document.querySelector(".sideCartMenu");
+let overlay = document.querySelector(".sidebar_overlay");
+
+function Open() {
+    sideCartMenu.style.width = "400px";
+    sideCartMenu.style.visibility = "visible";
+    sideCartMenu.style.opacity = "1";
+
+}
+
+function Close() {
+    sideCartMenu.style.width = "0px";
+    sideCartMenu.style.visibility = "hidden";
+    sideCartMenu.style.opacity = "0";
+
+}
+openCart.onclick = () => {
+    Open();
+    overlay.style.display = "block";
+}
+
+
+closeCart.onclick = () => {
+    Close();
+    overlay.style.display = "none";
+
+}
+window.onclick = (ev) => {
+    if (ev.target == overlay) {
+        Close();
+        overlay.style.display = "none";
+
+    }
+}
+
+
